0aefff2f4b1c1249a0351adcfd212d73e810f436
Chris <cshdevelopment1@gmail.com>
2021-03-17 21:09:52 -0400

    more stuff github

--- src/ForGitHub.jsx
+++ src/ForGitHub.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { makeStyles} from 'react'
+
 
 function ForGitHub() {
+    const [github, setGitHub] = useStyles();
     return (
+
+
+
         <div>
             This is for GitHub, so I don't Lose my Shhhhhheeeeeeit!
             <div className="thisIsDumb">
@@ -30,6 +35,70 @@ function ForGitHub() {
                 <h4 className="h4Stuff">And some more stuff here to write.</h4>
                 <h5 className="h5Stuff">And finally this stuff here</h5>
                 <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
                 <h4 className="h4Stuff">And some more stuff here to write.</h4>
                 <h5 className="h5Stuff">And finally this stuff here</h5>
                 <p>This is some random stuff here to write</p>
