eee3f2a611786e89bd1a7a232840d12895366621
Chris <cshdevelopment1@gmail.com>
2021-03-18 23:45:11 -0400

    newstuff for git component branch

--- src/ForGitHub.jsx
+++ src/ForGitHub.jsx
@@ -4,6 +4,11 @@ function ForGitHub() {
     return (
         <div>
             This is for GitHub, so I don't Lose my Shhhhhheeeeeeit!
+            <div className="thisIsDumb">
+                <p>This is some random stuff here to write</p>
+                <h4 className="h4Stuff">And some more stuff here to write.</h4>
+                <h5 className="h5Stuff">And finally this stuff here</h5>
+            </div>
         </div>
     )
 }
