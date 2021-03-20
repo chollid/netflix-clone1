f588ca6fad898b6534dd7634ddae47f792a79ebc
Chris <cshdevelopment1@gmail.com>
2021-03-19 23:25:16 -0400

    Files for github

---
+++ src/GitHub1.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function GitHub1() {
+    return (
+        <div>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+            <h4>More stuff here</h4>
+        </div>
+    )
+}
+ce
---
+++ src/NewGitHub.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function NewGitHub() {
+    return (
+        <div>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+            <h1>This si words</h1>
+        </div>
+    )
+}
+e
