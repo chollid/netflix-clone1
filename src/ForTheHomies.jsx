af5d505122856618420303601267c2025a132642
Chris <cshdevelopment1@gmail.com>
2021-03-20 23:55:51 -0400

    Added new page

---
+++ src/ForTheHomies.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function ForTheHomies() {
+    return (
+        <div>
+            <h4>
+            This one for the homies.
+            </h4>
+            <h4>
+            This one for the homies.
+            </h4>
+            <h4>
+            This one for the homies.
+            </h4>
+            <h4>
+            This one for the homies.
+            </h4>
+            <h4>
+            This one for the homies.
+            </h4>
+            <h4>
+            This one for the homies.
+            </h4>
+            <h4>
+            This one for the homies.
+            </h4>
+            <h4>
+            This one for the homies.
+            </h4>
+            <h4>
+            This one for the homies.
+            </h4>
+            <h4>
+            This one for the homies.
+            </h4>
+            <h4>
+            This one for the homies.
+            </h4>
+        </div>
+    )
+}
+
+export default ForTheHomies
