6a3c160022b62a249cc9843aaf04f7d0067bb796
Chris <cshdevelopment1@gmail.com>
2021-03-25 00:46:37 -0400

    Added new comp

---
+++ src/Homies2.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Homies2() {
+  return (
+    <div>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+      <h5>Do another thing</h5>
+    </div>
+  );
+}
+
+export default Homies2;
