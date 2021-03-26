df36df86c98694bf518386f538667f408d8b5d6d
Chris <cshdevelopment1@gmail.com>
2021-03-26 01:26:58 -0400

    added new file

--- src/Homies2.jsx
+++ src/Homies2.jsx
@@ -17,6 +17,91 @@ function Homies2() {
       <h5>Do another thing</h5>
       <h5>Do another thing</h5>
       <h5>Do another thing</h5>
+      <p>Hi</p>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+      <button>Do this</button>
+
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+      <input type="text" placeholder="Press me" />
+
+      <form action="POST/">
+        <input type="text" />
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
+      </form>
     </div>
   );
 }
