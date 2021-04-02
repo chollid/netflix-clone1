253bb0ce932163f69f3ed372f6be98c4db3eff4e
Chris <cshdevelopment1@gmail.com>
2021-04-01 23:58:59 -0400

    added file

--- src/GitStuff/NewThings.jsx
+++ src/GitStuff/NewThings.jsx
@@ -4,7 +4,13 @@ import './GitStuff.css';
 
 const thisThing = 89;
 function NewThings() {
-  return <div></div>;
+  <div>
+    {thisThing + 88}
+
+    <p>Watch this number it's called additon. </p>
+    <h1>Press this button</h1>
+    <button>Press Me</button>
+  </div>;
 }
 
 export default NewThings;
