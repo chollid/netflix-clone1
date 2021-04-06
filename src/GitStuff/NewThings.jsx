1a53d75b5fb2224e7f48a09fda751ace3f9a92e9
Chris <cshdevelopment1@gmail.com>
2021-04-05 21:15:43 -0400

    New form

--- src/GitStuff/NewThings.jsx
+++ src/GitStuff/NewThings.jsx
@@ -12,36 +12,41 @@ function NewThings() {
       <input type="email" placeholder="bob's menu" />
       <p>Hey Look up above!</p>
     </form>
-
-    {77 + 99}
-    {77 + 99}
-    {277 + 9988}
-    {77 + 99}
-    {767 + 9988}
-    {77 + 9889}
-    {877 + 91119}
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
-    <button>Pres Me</button>
-    <h2>Go away from me!</h2>
+    <form action="POST">
+      <button>Help Me</button>
+      <input type="email" placeholder="bob's menu" />
+      <p>Hey Look up above!</p>
+    </form>
+    <form action="POST">
+      <button>Help Me</button>
+      <input type="email" placeholder="bob's menu" />
+      <p>Hey Look up above!</p>
+    </form>
+    <form action="POST">
+      <button>Help Me</button>
+      <input type="email" placeholder="bob's menu" />
+      <p>Hey Look up above!</p>
+    </form>
+    <form action="POST">
+      <button>Help Me</button>
+      <input type="email" placeholder="bob's menu" />
+      <p>Hey Look up above!</p>
+    </form>
+    <form action="POST">
+      <button>Help Me</button>
+      <input type="email" placeholder="bob's menu" />
+      <p>Hey Look up above!</p>
+    </form>
+    <form action="POST">
+      <button>Help Me</button>
+      <input type="email" placeholder="bob's menu" />
+      <p>Hey Look up above!</p>
+    </form>
+    <form action="POST">
+      <button>Help Me</button>
+      <input type="email" placeholder="bob's menu" />
+      <p>Hey Look up above!</p>
+    </form>
   </div>;
 }
 
