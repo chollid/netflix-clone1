cf24d0b94e8a31a4d11241cfe893587a52d1b905
Chris <cshdevelopment1@gmail.com>
2021-04-07 14:12:00 -0400

    newthings

---
+++ src/GitStuff/ThoseThings.jsx
@@ -0,0 +1,10 @@
+const momma = function dude(x) {
+  console.log(`${x}, yea that's correct.`);
+};
+
+const func1 = (y) => {
+  momma('Hi');
+  console.log(`${momma}, Oh yea!`);
+};
+
+func1('deeznuts');
