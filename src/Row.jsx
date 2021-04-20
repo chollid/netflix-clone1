23d20b1596b21896bd0748095da26cc1de189b66
chollid <cshdevelopment1@gmail.com>
2021-04-20 01:14:07 -0400

    Clean now

--- src/Row.jsx
+++ src/Row.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import './Row.css';
 
-function Row({ title, fetchUrl, isLargeRow }) {
+function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
 
   const base_url = 'https://image.tmdb.org/t/p/original/';
@@ -21,20 +21,16 @@ function Row({ title, fetchUrl, isLargeRow }) {
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
-        {movies.map(
-          (movie) =>
-            (isLargeRow && movie.poster_path) ||
-            (!isLargeRow && movie.backdrop_path && (
-              <img
-                className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
-                key={movie.id}
-                src={`${base_url}${
-                  isLargeRow ? movie.poster_path : movie.backdrop_path
-                }`}
-                alt={movie.name}
-              />
-            ))
-        )}
+        {movies.map((movie) => (
+          <img
+            className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+            jet={movie.id}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
       </div>
     </div>
   );
