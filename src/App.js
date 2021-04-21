d66bc24d1c1ab3d11cd4481a12e74303668abaa6
chollid <cshdevelopment1@gmail.com>
2021-04-21 00:25:15 -0400

    Added redux store, bug with useSelector, state is undefined

--- src/App.js
+++ src/App.js
@@ -2,25 +2,38 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
 import LoginScreen from './LoginScreen';
 import HomeScreen from './screens/HomeScreen.jsx';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
-  const user = null;
+  // Pull in the user State from the redux store
+  const user = useSelector(selectUser);
+  // const user = null;
+  //Shoot a payload (dispatch) to the redux store
+  const dispatch = useDispatch();
 
   // LISTENER  To Check for change in userAuth logged-in/logged-out
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         //logged in
-        console.log(userAuth.uid);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+        console.log(user);
       } else {
         //logged out
+        dispatch(logout);
       }
     });
 
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
   return (
     <div className="app">
       <Router>
