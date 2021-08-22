a9857875a88b26e557b6f0b1bde45bbaf80c259a
chollid <chrisuga@me.com>
2021-08-22 17:53:28 -0400

    Axios def

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+//Axios is a promise based HTTP client for the browser and Node. js. Axios makes it easy to send asynchronous HTTP requests to REST endpoints and perform CRUD operations. It can be used in plain JavaScript or with a library such as Vue or React
+
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
+  baseURL: 'https://api.themoviedb.org/3',
 });
 
 export default instance;
