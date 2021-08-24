63041b71a5e46adefaccbdb2b16a8127667276f0
chollid <chrisuga@me.com>
2021-08-23 20:23:43 -0400

    Store def

--- src/app/store.js
+++ src/app/store.js
@@ -1,3 +1,7 @@
+//A store holds the whole state tree of your application. The only way to change the state inside it is to dispatch an action on it.
+
+//A store is not a class. It's just an object with a few methods on it. To create it, pass your root reducing function to createStore.
+
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
 
